b444d0047b6e70403ef0867609768b4405040e9e
alissontibes <alisson.tibes@live.com>
2025-08-11 15:18:56 -0300

    Create variables.tf

---
+++ modules/route-tables/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {}
+variable "public_subnet_ids" {
+  type = list(string)
+}
+variable "private_subnet_ids" {
+  type = list(string)
+}
+variable "igw_id" {}
+variable "nat_gateway_id" {}
