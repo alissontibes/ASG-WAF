36aeb49db8d515755a5de59db203e5388fe8c1b3
alissontibes <alisson.tibes@live.com>
2025-08-11 15:20:01 -0300

    Create main.tf

---
+++ modules/security-groups/main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "default_sg" {
+  name        = "waf-sg"
+  description = "WAF Default security group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
