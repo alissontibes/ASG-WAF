f148a23eab78f638d3d095f737d431c2b76ec02c
alissontibes <alisson.tibes@live.com>
2025-08-11 15:15:20 -0300

    Create variables.tf

---
+++ modules/subnets/variables.tf
@@ -0,0 +1,10 @@
+variable "vpc_id" {}
+variable "public_subnets" {
+  type = list(string)
+}
+variable "private_subnets" {
+  type = list(string)
+}
+variable "availability_zones" {
+  type = list(string)
+}
