62922dfacd3b1ae79b80eee0a1d926a6579e3187
alissontibes <alisson.tibes@live.com>
2025-08-11 16:25:46 -0300

    Update outputs.tf

--- modules/security-groups/outputs.tf
+++ modules/security-groups/outputs.tf
@@ -1,4 +1,4 @@
 output "waf_sg_id" {
   description = "Security Group ID for WAF instances"
-  value       = aws_security_group.waf_sg.id  # Make sure the resource name matches your SG resource
+  value       = aws_security_group.waf.id  # Make sure the resource name matches your SG resource
 }
