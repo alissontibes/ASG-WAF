710f6559c543fe7dbd49d62591b38614dc6e3640
alissontibes <alisson.tibes@live.com>
2025-08-11 16:38:16 -0300

    Update main.tf

--- modules/cloudguard-waf/main.tf
+++ modules/cloudguard-waf/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "waf" {
   ami           = var.waf_ami_id
-  instance_type = var.instance_type
+  instance_type = var.waf_instance_type
   subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   key_name      = var.key_name
