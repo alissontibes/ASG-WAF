177cffda9ed966aa5a60b79560b1d97d02d5a338
alissontibes <alisson.tibes@live.com>
2025-08-11 16:25:07 -0300

    Update main.tf

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "default_sg" {
+resource "aws_security_group" "waf" {
   name        = "waf-sg"
   description = "WAF Default security group"
   vpc_id      = var.vpc_id
